524bbc38b930f4faa8a654393f07f9a878fe3fac
Oktay Tanrıkulu <oktaytanrikulu@gmail.com>
2023-03-23 15:30:52 +0300

    App created and added app icons.

---
+++ PokeApp/PokeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeApp
+//
+//  Created by Oktay Tanrıkulu on 23.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
